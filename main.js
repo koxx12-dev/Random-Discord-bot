86569b507398e4bf89571b498738067d9e393209
koxx12-dev <80456351+koxx12-dev@users.noreply.github.com>
2021-06-02 17:41:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Botloader = require('./classes/BotLoader');
 
 const client = new Botloader();
-client.start('ODQ5NjQxMTI5Mzk2Nzk3NTAy.YLeH9g.amm-qx79Q6x2gJnHpwMOy6L_0lU');
+client.start('token');
 
 process.on('unhandledRejection', err => console.error(err));
